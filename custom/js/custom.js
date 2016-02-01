429d598be94e7de14ac90a415db68c0d0735b1f8
viktor_chayka <viktor.chaika@ed-era.com>
2016-02-01 14:39:32 +0200

    Update custom/js/custom.js

---
+++ custom/js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  function init () {
+    var href, $lastLink;
+
+    $("a.toggle-search").remove();
+
+    $lastLink = $(".custom-link:last");
+    href = $lastLink.prop("href");
+    href = "https://ed-era.com/books/" + _.last(href.split("-"));
+    $lastLink.prop("href", href);
+  }
+  init();
+  require(["gitbook"], function (gitbook) {
+    gitbook.events.bind("page.change", init);
+  });
+});
