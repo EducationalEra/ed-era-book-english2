a2518f4db141afecdac509941c8065059ef02635
viktor_chayka <viktor.chaika@ed-era.com>
2016-02-12 17:42:07 +0200

    Update custom/js/popup.js

---
+++ custom/js/popup.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   function init () {
+      $( "div.popup" ).replaceWith( "<div class='description'><div class='text'>Ще більше відеоматеріалів в <a class='knopka1' href='https://study.ed-era.com/courses/EdEra/m102/M102/about' target='_blank'>онлайн-курсі</a> від розробників цієї книги</div><div class='btn-knopka'><a class='knopka' href='#'   target='_blank'>Перейти на курс</a></div>" );
+   }
+
+   init();
+   require(["gitbook"], function (gitbook) {
+      gitbook.events.bind("page.change", init);
+   });
+});
